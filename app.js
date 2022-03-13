8114a4a7b6f74f8dce1d384ca0dc395be91bc534
Tomas Sanchez <tomassanchezmedina92@gmail.com>
2022-03-13 19:22:22 +0100

    chore: changing some styles and optimizing code

--- app.js
+++ app.js
@@ -7,16 +7,93 @@ const init = async () => {
 }
 
 const goPokedex = () => {
-    const main = document.querySelector('.main');
-    removeAllChilds(main);
-    main.innerHTML = `<div class="pokeapiDiv"><img class ="pokeapiIMG" src="https://avatars.githubusercontent.com/u/64151210?v=4" alt="letras pokemon"></div><div class="filterContent"><input class ="filterInput" id="filterInput" type="text" placeholder="Escribe aquí el nombre de un pokemon para saber mas sobre el"><button  onclick="getFiltered()">Filtrar</button><button  onclick="resetFilter()">Limpiar filtro</button></div><div><ul class="filteredPokemons"></ul></div><div><ul class="pokemons"></ul></div>"`
+    const body = document.querySelector('body');
+    body.className = 'pokedexBody';
+    removeAllChilds(body);
+    body.innerHTML = `<header class="header"></header><main class="pokedexMain"></main>`
+    createNav();
+    pokedexMain();
     getAllMapped();
 }
 
-const goLaboratorio = () => {
-    const main = document.querySelector('.main');
-    removeAllChilds(main);
+const goLaboratory = () => {
+    const body = document.querySelector('body');
+    body.className = 'laboratoryBody';
+    removeAllChilds(body);
+    body.innerHTML = `<header class="header"></header><main class="laboratoryMain"></main>`
+    createNav();
+    labolatoryMain();
 }
+
+const goMinigame = () => {
+    const body = document.querySelector('body');
+    body.className = 'minigameBody';
+    removeAllChilds(body);
+    body.innerHTML = `<header class="header"></header><main class="minigameMain"></main>`
+    createNav();
+    minigameMain();
+}
+
+const createNav = () => {
+    const navBar = document.querySelector('.header');
+    navBar.innerHTML = 
+    `<nav>
+        <ul class="navigation">
+            <button onclick="goPokedex()" class="navigation_button">Pokedex</button>
+            <button onclick="goLaboratory()" class="navigation_button">Laboratorio</button>
+            <button <button onclick="goMinigame()"class="navigation_button">Minijuego</button>
+        </ul>
+    </nav>`
+}
+
+const pokedexMain = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = 
+    `<div class="pokeapiDiv">
+        <img class ="pokeapiIMG" src="https://avatars.githubusercontent.com/u/64151210?v=4" alt="letras pokemon">
+    </div>
+    <div class="filterContent">
+        <input class ="filterInput" id="filterInput" type="text" placeholder="Escribe aquí el nombre de un pokemon para saber mas sobre el">
+        <button  onclick="getFiltered()">Filtrar</button>
+        <button  onclick="resetFilter()">Limpiar filtro</button>
+    </div>
+    <div>
+        <ul class="filteredPokemons"></ul>
+    </div>` 
+}
+
+const labolatoryMain = () =>{
+    const main = document.querySelector('main');
+    main.innerHTML = 
+    `<div class="todoDiv">
+        <h1 class="laboratoryTitle">TO DO LIST</h1>
+        <input class="toDoInput" type="text">
+        <button onclick="toDoItem()" class="laboratoryButton">Apuntar</button>
+        <ul class="toDoList"></ul>
+    </div>`;
+}
+
+/* const minigameMain = () => {
+    const main = document.querySelector('main');
+    main.innerHTML = 
+        `<h1>Whack A Mole</h1>
+	    <h2>your score: </h2>
+	    <h2 class="score">0</h2>
+	    <div>Seconds left:</div>
+	    <h2 id="time-left">60</h2>
+        <button onclick="startGame()"
+	    <div class="grid">
+		    <div class="square" id="1"></div>
+		    <div class="square mole" id="2"></div>
+		    <div class="square" id="3"></div>
+		    <div class="square" id="4"></div>
+		    <div class="square" id="5"></div>
+		    <div class="square" id="6"></div>
+		    <div class="square" id="7"></div>
+		    <div class="square" id="8"></div>
+		    <div class="square" id="9"></div>
+	    </div>`
+} */
 
 const getPokemon = async (id) => {
     const result = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
@@ -44,13 +121,15 @@ const getAllMapped = async () => {
             number: pokemon.order
         }
     })
+    const allPokemonsDiv = document.createElement('div');
+    const allPokemonsList = document.createElement('ul');
+    allPokemonsList.className = 'pokemons'
     mappedPokemons.forEach(pokemon => {
-        const pokemonsList = document.querySelector(".pokemons")
-        const pokemonCard = document.createElement('li');
-        pokemonCard.className = "pokemonCard";
-        pokemonCard.innerHTML = `<img src="${pokemon.image}" alt="${pokemon.name}"><h2 class="pokemonName">${pokemon.name}</h2><p class="mappedCardP">Altura: ${pokemon.height}m</p><p class="mappedCardP">Peso: ${pokemon.weight}kg</p><p class="mappedCardP"># ${pokemon.number}</p>`
-        pokemonsList.appendChild(pokemonCard);
+    allPokemonsList.innerHTML += `<li class="pokemonCard"><img src="${pokemon.image}" alt="${pokemon.name}"><h2 class="pokemonName">${pokemon.name}</h2><p class="mappedCardP">Altura: ${pokemon.height}m</p><p class="mappedCardP">Peso: ${pokemon.weight}kg</p><p class="mappedCardP"># ${pokemon.number}</p></li>`
     });
+    const main = document.querySelector('main');
+    main.appendChild(allPokemonsDiv).appendChild(allPokemonsList);
+   
 }
 
 const getFiltered = async () => {
@@ -58,18 +137,24 @@ const getFiltered = async () => {
     const inputFilter = document.querySelector('#filterInput')
     const filteredPokemons = pokemons.filter(pokemon => (pokemon.name.toUpperCase() == inputFilter.value.toUpperCase()));
     filteredPokemons.forEach(pokemon => {
+        /* let filteredList = document.querySelector(".filteredPokemons")
+        if (filteredList != null){
+            const filtered = document.createElement('ul');
+            filtered.className = ".filteredPokemons";
+            filteredList = document.querySelector(".filteredPokemons")
+        } */
         const filteredList = document.querySelector(".filteredPokemons")
         const pokemonCard = document.createElement('li');
+        pokemonCard.className = "filteredCard"
+        pokemonCard.innerHTML = `<div class="filteredCardImg" ><img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"></div><div class ="filteredUl"><ul class="filteredCardText${pokemon.name}"></ul></div>`
+        filteredList.appendChild(pokemonCard);
+        const filteredText = document.querySelector(`.filteredCardText${pokemon.name}`);
         const mappedStats = pokemon.stats.map(stat =>{
             return {
                 statName: stat.stat.name,
                 statValue: stat.base_stat
             }
         })
-        pokemonCard.className = "filteredCard"
-        pokemonCard.innerHTML = `<div class="filteredCardImg" ><img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}"></div><div class ="filteredUl"><ul class="filteredCardText${pokemon.name}"></ul></div>`
-        filteredList.appendChild(pokemonCard);
-        const filteredText = document.querySelector(`.filteredCardText${pokemon.name}`);
         for (const stat of mappedStats) {
             filteredText.innerHTML += `<li>${stat.statName.toUpperCase()} =>   ${stat.statValue}</li>`
            }
@@ -91,3 +176,67 @@ const resetFilter = () => {
     getAllMapped();
 }
 
+const toDoItem = () => {
+    const inputText = document.querySelector('.toDoInput')
+    const toDoListItem = document.createElement('li');
+    toDoListItem.innerHTML = `<p class="toDoListText">${inputText.value}</p>`
+    let removeLastButton = document.createElement('button');
+    removeLastButton.textContent = 'X';
+    removeLastButton.addEventListener('click', () => removeLast(toDoListItem));
+    const toDoList = document.querySelector('.toDoList');
+    toDoList.appendChild(toDoListItem);
+    toDoListItem.appendChild(removeLastButton);
+    inputText.value = "";
+}
+
+const removeLast = (item) => {
+    item.remove();
+}
+
+/* const startGame = () => {
+const square = document.querySelectorAll('.square');
+const mole = document.querySelectorAll('.mole');
+const timeleft = document.querySelector('#time-left');
+const score = document.querySelector('#score');
+
+let results = 0;
+let currentTime = timeleft.textContent;
+
+function randomSquare(){
+	square.forEach( className => {
+		className.classList.remove('mole');
+	});
+	let randomPosition = square[Math.floor(Math.random() * 9)];
+	randomPosition.classList.add('mole');
+
+	hitPosition = randomPosition.id;
+}
+
+square.forEach( id => {
+	id.addEventListener('mouseup', () => {
+		if(id.id === hitPosition){
+			results = results + 1;
+			score.textContent = results;
+		}
+	});
+});
+
+function moveMole(){
+	let timerId=null;
+	timerId = setInterval(randomSquare, 1000);
+}
+
+moveMole();
+
+function countDown(){
+	currentTime--;
+	timeleft.textContent = currentTime;
+	if(currentTime===0){
+		clearInterval(timerId);
+		alert('Game Over! your final score is: '+ results);
+	}
+}
+
+
+let timerId = setInterval(countDown, 1000);
+} */
